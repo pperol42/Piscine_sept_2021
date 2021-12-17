6f5ec11882852f3cfa12563b16d3a0c825be8dc6
pperol42 <96288985+pperol42@users.noreply.github.com>
2021-12-17 14:19:40 +0100

    1st commit

---
+++ C_05/ex02/ft_iterative_power.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pperol <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/28 17:32:37 by pperol            #+#    #+#             */
+/*   Updated: 2021/09/29 10:09:21 by pperol           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	result;
+
+	result = nb;
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	while (power-- > 1)
+		result *= nb;
+	return (result);
+}
---
+++ C_05/ex07/ft_find_next_prime.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pperol <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/29 17:03:01 by pperol            #+#    #+#             */
+/*   Updated: 2021/09/29 18:41:17 by pperol           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	i = 2;
+	if (nb < 2)
+		return (0);
+	while (i <= nb / i)
+	{
+		if (nb % i == 0)
+			return (0);
+		else if (i == 2)
+			i++;
+		else
+			i += 2;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	int	i;
+
+	i = 0;
+	if (nb <= 2)
+		return (2);
+	while (!ft_is_prime(nb + i))
+		i++;
+	return (nb + i);
+}
