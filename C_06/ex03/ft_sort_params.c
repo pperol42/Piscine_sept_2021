0dbbff991d4f54b7cd8f9c610e4b82edf2be7c42
pperol42 <96288985+pperol42@users.noreply.github.com>
2021-12-17 14:28:35 +0100

    1st commit

---
+++ C_06/ex03/ft_sort_params.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pperol <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/30 13:50:02 by pperol            #+#    #+#             */
+/*   Updated: 2021/09/30 14:14:09 by pperol           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	n;
+
+	n = 0;
+	while (s1[n] && s2[n] && s1[n] == s2[n])
+		n++;
+	return (s1[n] - s2[n]);
+}
+
+int	main(int argc, char *argv[])
+{
+	int		i;
+	char	*tmp;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+		{
+			tmp = argv[i];
+			argv[i] = argv[i + 1];
+			argv[i + 1] = tmp;
+			i = 0;
+		}
+		else
+			i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
